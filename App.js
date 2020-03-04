df5287a8efce5ba5f1c4ce6980b3fa47c309b29c
jadekang <jkang.se@gmail.com>
2020-03-03 20:37:29 -0800

    added adding and removing handlers

--- App.js
+++ App.js
@@ -1,28 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList
+} from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  // function goalInputHandler(enteredText) {
+  //   setEnterGoal(enteredText);
+  // }
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: goalTitle }
+    ]);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.id !== goalId);
+    });
+  };
+
   return (
     <View style={styles.screen}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center'
-        }}
-      >
-        <TextInput
-          placeholder="Course Goal"
-          style={{
-            width: '80%',
-            borderColor: 'black',
-            borderWidth: 1,
-            padding: 10
-          }}
-        />
-        <Button title="ADD" />
-      </View>
-      <View></View>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={() => console.log('Does this get clicked?')}
+            title={itemData.item.value}
+          />
+        )}
+      />
     </View>
   );
 }
