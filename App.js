cedb6b2955b6f30d4fcfca209694517a4c41f19b
jadekang <jkang.se@gmail.com>
2020-03-03 19:00:20 -0800

    the first reactive native app

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            width: '80%',
+            borderColor: 'black',
+            borderWidth: 1,
+            padding: 10
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  }
+});
